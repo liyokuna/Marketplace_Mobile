f32eb05608abc4960e39606f623f1aa47100131d
Lionel MUKUNA CIOWELA <liyokuna@yahoo.fr>
2017-02-14 18:37:37 +0100

    So far

--- src/pages/bon/bon.ts
+++ src/pages/bon/bon.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams,ToastController  } from 'ionic-angular';
 
 /*
   Generated class for the Bon page.
@@ -13,7 +13,16 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class BonPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {}
+ 
+ showToast(position: string) {
+    let toast = this.toastCtrl.create({
+      message: 'Le bon a été ajouté dans votre panier',
+      duration: 3000,
+      position: position
+    });
+    toast.present();
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BonPage');
--- src/pages/marche/marche.ts
+++ src/pages/marche/marche.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController  } from 'ionic-angular';
 
 /*
   Generated class for the Marche page.
@@ -13,7 +13,15 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class MarchePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController) {}
+
+ presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Le bon a été ajouté dans votre panier',
+      duration: 3000
+    });
+    toast.present();
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MarchePage');
